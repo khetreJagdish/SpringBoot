fdd880801dfedd2f7b776fcf406c3275f5ee1d41
khetreJagdish <30476034+khetreJagdish@users.noreply.github.com>
2022-04-10 21:47:07 +0530

    Update UserResource.java

--- restful-web-services/src/main/java/com/springbootexample/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/springbootexample/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,11 +1,16 @@
 package com.springbootexample.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -21,5 +26,16 @@ public class UserResource {
 	@GetMapping("/user/{id}")
 	public User retriveUser(@PathVariable int id) {
 		return userDaoService.findOne(id);
+	}
+	
+	@PostMapping("/user")
+	public ResponseEntity<Object> createUser(@RequestBody User  user) {
+		User savedUser = userDaoService.saveUser(user);
+		
+		URI location =  ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
