dda80bf212e10933bffc2848d0ddfa949d7d3c93
khetreJagdish <30476034+khetreJagdish@users.noreply.github.com>
2022-04-10 20:03:24 +0530

    commit for Spring Boot App
    commit for Spring Boot App

---
+++ restful-web-services/src/main/java/com/springbootexample/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.springbootexample.rest.webservices.restfulwebservices;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+
+}
---
+++ restful-web-services/src/main/java/com/springbootexample/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.springbootexample.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		
+		return "Hello World from Get";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		
+		return new HelloWorldBean("Hello World from Get Bean");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		
+		return new HelloWorldBean(String.format("Hello World from Get PathVariable, %s", name));
+	}
+}
